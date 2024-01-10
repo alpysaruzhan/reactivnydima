f30f5b111eec197f85a85f33863533c925cb556b
pikoUsername <galymzhan.amantaj@gmail.com>
2024-01-10 15:28:06 +0600

    charactersics parsing is now working

--- src/Sell/GameCategory.js
+++ src/Sell/GameCategory.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, Link } from 'react-router-dom'; 
+import { useParams, Link, useNavigate } from 'react-router-dom'; 
 import "./SellPage.css";
 import { MarketApi, ProductApi } from 'market_place';
 import { Instance, basePath } from '../GateWay/base';
+import { RangeCharactersitics, SwitchCharactersitics, SelectorCharactersitics } from './components/characteristicsButtons';
 
 const GameCategoryPag = () => {
     const { id } = useParams();
     const marketAPI = new MarketApi(Instance);
     const [options, setOptions] = useState([]);
+    const [attributes, setAttributes] = useState([]);  // ????? очистится ли он?  
+    const {navigate} = useNavigate()
+
+    const addAttributes = (label, value) => { 
+        setAttributes([...attributes, {label: value}])
+    }
+
+    const HandleSelectedCharcs = () => { 
+        // потом при отправке формы отправить это вместе! 
+        localStorage.setItem("productAttributes", JSON.stringify(attributes))
+
+        setTimeout(() => navigate("/"), 500)
+    }
 
     useEffect(() => {
         marketAPI.categoryGetCategoryByIdApiV1CategoryCategoryIdIdGet(id, (error, data, response) => {
@@ -22,14 +36,10 @@ const GameCategoryPag = () => {
 
     return (
         <div className='cont8'>
-            {options.map((option) => {
-                switch (option.type) {
-                    case "SELECTOR":   return "#FF0000";
-                    case "SWITCH": return "#00FF00";
-                    case "RANGE":  return "#0000FF";
-                    default:      return (<div></div>);
-                }
-            })}
+            <SelectorCharactersitics options={options} addAttributes={addAttributes}/>
+            <RangeCharactersitics options={options} addAttributes={addAttributes}/>
+            <SwitchCharactersitics options={options} addAttributes={addAttributes}/>
+            <button onClick={HandleSelectedCharcs}>Отправить</button>
         </div>
     );
 }
--- src/Sell/components/characteristicsButtons.js
+++ src/Sell/components/characteristicsButtons.js
@@ -1,51 +1,90 @@
-const SwitchButton = (props) => {
-    const { defaultState } = props 
-    
-    return (
-        <div>
-            {/* make button here!! */}
-        </div>
-    )
+export const OptionTypes = { 
+    RANGE: 'range', 
+    SWITCH: 'switch', 
+    SELECTOR: 'selector'
 }
 
-const SelectButton = (props) => {
-    // ButtonName это 
-    const { buttonName } = props; 
-    
-    return ( 
-        <div>
-
-        </div>
-    )
-}
-
-const SelectorButtons = (props) => { 
-    // фильтруются только опции которые имеют тип - SELECTOR 
-    const { options } = props 
+function getUniqueGroups(arr) {
     let uniqueGroups = []
-    options.forEach((option) => {
-        if (uniqueGroups.includes(option) !== null || uniqueGroups.includes(option) !== undefined) { 
-            uniqueGroups.push(option)
+    arr.forEach((option) => {
+        if (!uniqueGroups.includes(option.group)) { 
+            uniqueGroups.push(option.group)
         }
     })
+    return uniqueGroups
+}
+
+function filterOptionsByType(options, type) { 
+    let result = []
+
+    options.forEach((option) => {
+        if (option.type === type.toUpperCase()) { 
+            result.push(option)
+        }
+    })
+    return result
+}
+
+
+export const RangeCharactersitics = (props) => { 
+    // Предпологается что структуры данных правильны скпонованы изначально! 
+    const { options, addAttributes } = props; 
+    let rangeOptions = filterOptionsByType(options, OptionTypes.RANGE)
+
+    return ( 
+        <div>
+            {rangeOptions.map((option) => { 
+                return ( 
+                    <div>
+                        <input type="number" placeholder={option.valueRange.min + " До " + option.valueRange.max}>Введите значение</input>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+
+export const SwitchCharactersitics = (props) => {
+    const { options, addAttributes } = props; 
+    let switchOptions = filterOptionsByType(options, OptionTypes.SWITCH)
+
+    return (
+        <div>
+            {switchOptions.map((option) => {
+                return ( 
+                    <div>
+                        <p>{option.label}</p>
+                        {/* Красивое переключение True False 
+                        и ставит значение через функцию addAttributes(option.field, и значение) */}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export const SelectorCharactersitics = (props) => { 
+    const { options, addAttributes } = props 
+    let uniqueGroups = getUniqueGroups(options)
+    let selectorOptions = filterOptionsByType(options, OptionTypes.SELECTOR)
 
     return ( 
         <div>
             {uniqueGroups.map((group) => {
                 <h3>{ group }</h3>
                 {/* дается несколько характеристик типо 20 гемов, 40 гемов и т.д. на выбор и можно выбрать только один */}
-                return (options.map((option) => {
+                return (selectorOptions.map((option) => {
                     return ( 
                         <div>
                             <div>{option.label}</div>
-                            {/* Красивая кнопка */}
+                            {/* Красивый выбор через кнопки только одного выбора, потом
+                            ставит значение через функцию addAttributes(option.field, и выбранное значение) */}
                         </div>
                     )
                 })) 
-                }
-            )
+                })
             }
-            
         </div>
     )
 }
