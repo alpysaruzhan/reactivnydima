4c9ff1397be602426b8f7d67d53c8180c7831998
alpysaruzhan <108935473+alpysaruzhan@users.noreply.github.com>
2024-02-01 13:03:46 +0600

    prodabgt

--- src/Header.js
+++ src/Header.js
@@ -3,34 +3,45 @@ import { NavLink } from "react-router-dom";
 import image from "./img/image37.svg";
 import logowithout from "./img/logowithout.png";
 import logo from "./img/logowhit.png";
+import { useState } from 'react';
+import "./Header.css";
 import { TEMP_EMAIL_KEY } from "./GateWay/consts";
 import { Cookies } from "react-cookie";
-import "./Header.css";
 
-const Header = ({ authorized }) => {
-
-  console.log(authorized);
+  
+const Header = ({authorized}) => {
+  console.log(authorized)
 
   return (
-    <header>
+    <header className="header">
       <div className="mobile">
-        <div>
-          <div className="logo">
-          </div>
-        </div>
+        <div><div className="logo">
+          
+          {/* <NavLink to="/">
+            <img className="logo-img" src={logo} alt="" />
+            <img className="logo-img2" src={logowithout} alt="" />
+          </NavLink> */}
+        </div></div>
       </div>
+      {/* <div className="header-input">
+        <p className="pi">
+          <img className="re" src={image} alt="" />
+          <input placeholder="Поиск игр в приложении" type="text" />
+        </p>
+      </div> */}
       <div>
-        {!authorized ? (
-          <div className="header-buttons">
-            <NavLink to="/login/code" className="vhod">
-              <button className="header-button1">Вход</button>
-            </NavLink>
-            <NavLink to="/login/code" className="registracia">
-              <button className="header-button2">Регистрация</button>
-            </NavLink>
-          </div>
-        ) : null}
+        
+      
+        {!authorized ?  <div className="header-buttons">
+        <NavLink to="/login/code" className="vhod">
+          <button className="header-button1">Вход</button>
+        </NavLink>
+        <NavLink to="/login/code"className="registracia">
+          <button className="header-button2">Регистрация</button>
+        </NavLink>
+      </div> : null}
       </div>
+     
     </header>
   );
 };
